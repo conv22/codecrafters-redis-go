aba39b1e5d2844d58c29018c5471d3e04eb82d8c
Emil Egiazarov <eegiazarov@deem.com>
2024-02-07 22:34:57 +0700

    parse resp

--- app/server.go
+++ app/server.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
+	"sync"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!!!")
-
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
@@ -19,43 +19,95 @@ func main() {
 	}
 	defer listener.Close()
 
+	var wg sync.WaitGroup
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error:", err)
 			continue
 		}
-		go handleClient(conn)
+		wg.Add(1)
+		go handleClient(conn, &wg)
 	}
 }
 
-func handleClient(conn net.Conn) {
-	defer conn.Close()
+func parseResp(s string) ([]string, error) {
+	clone := strings.Clone(s)
+	const TERMINATOR = "\r\n"
+	const ITEM_COUNT = "*"
+	const ITEM_LENGTH = "$"
 
-	scanner := bufio.NewScanner(conn)
+	first_item_index := strings.Index(clone, TERMINATOR)
+
+	if first_item_index == -1 {
+		return nil, errors.New("not valid string")
+	}
+
+	total_items_count, err := strconv.Atoi(clone[1:first_item_index])
+
+	if err != nil {
+		return nil, err
+	}
+
+	clone = clone[(first_item_index + len(TERMINATOR)):]
+	curr_count := 0
+	curr_item_length := 0
+	words := make([]string, 0, total_items_count)
+
+	for curr_count < total_items_count {
+		if strings.HasPrefix(clone, ITEM_LENGTH) {
+			first_item_index := strings.Index(clone, TERMINATOR)
+
+			total_chars_count, err := strconv.Atoi(clone[1:first_item_index])
+			if err != nil {
+				return nil, err
+			}
+			clone = clone[first_item_index+len(TERMINATOR):]
+			curr_item_length = total_chars_count
+			continue
+		}
+		words = append(words, clone[0:curr_item_length])
+		clone = clone[(len(TERMINATOR) + curr_item_length):]
+		curr_count++
+	}
+
+	return words, nil
+
+}
+
+func handleClient(conn net.Conn, wg *sync.WaitGroup) {
+	defer conn.Close()
+	defer wg.Done()
+
+	reader := bufio.NewReader(conn)
 	writer := bufio.NewWriter(conn)
 
 	PING_RESPONSE := "+PONG\r\n"
+	buffer := make([]byte, 1024)
+	for {
+		n, err := reader.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading from connection:", err)
+			return
+		}
 
-	for scanner.Scan() {
-		input := scanner.Text()
+		input := string(buffer[:n])
+
+		input = strings.TrimSpace(input)
 
 		if strings.ToLower(input) == "ping" {
 			writer.WriteString(PING_RESPONSE)
-		} else if strings.HasPrefix(strings.ToLower(input), "echo") {
-			result := strings.TrimPrefix(input, "echo")
-			writer.WriteString(result)
+		} else if values, err := parseResp(input); err == nil {
+			if len(values) == 2 && strings.ToLower(values[0]) == "echo" {
+				msg := "+" + values[1] + "\r\n"
+				writer.WriteString(msg)
+			}
 		}
 
 		if err := writer.Flush(); err != nil {
 			fmt.Println("Error flushing writer:", err)
 			return
 		}
-
 	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error scanning user input")
-	}
-
 }
